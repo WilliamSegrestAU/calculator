a5c1f861ac3b63f3918b27ae3ecb9258d112bb72
WilliamSegrestAU <wbs0026@auburn.edu>
2022-10-15 13:38:15 -0500

    Designed logic for calculator
    Removed original functions for adding, subtracting, etc.
    
    Operate function serves purpose for calculating two numbers according to operator.
    
    Each button on calculator has appointed value.
    
    Event listener looks for button value and puts target value in the calculate function based on conditions.
    
    Calculate function checks for operator, if found and not "=", it is stored into opValue.
    
    If opValue has not been stored yet, the first value (a) is stored until operator is found.
    
    If the event target is not an operator but first value has been stored, the second value is configured (b).
    
    Each calculation is put as the display value for the user to see.

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
-function add(a, b) {
-    return a + b;
-}
+let opValue = '';
+let firstValue = '';
+let step = 0;
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
+const operators = ['+', '-', '*', '/', '='];
 
 function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
         case '+':
-            return add(a, b);
+            return a + b;
         case '-':
-            return subtract(a, b);
+            return a - b;
         case '*':
-            return multiply(a, b);
+            return a * b;
         case '/':
-            return divide(a, b);
+            return a / b;
         default:
             break;
     }
 }
+
+function calulate(val) {
+    // Check for operator
+    // Operator stored
+    if (operators.includes(val)) {
+        if (val == '=') {
+            display.value = operate(opValue, firstValue, secondValue);
+            firstValue = display.value;
+            secondValue = '';
+        }
+        else if (opValue != '' && secondValue != '') {
+            // Must be evaluated, first value is stored as operate
+            display.value = operate(opValue, firstValue, secondValue);
+            firstValue = display.value;
+            secondValue = '';
+            opValue = val;
+        }
+        else {
+            opValue = val;
+        }
+    }
+    else {
+        // First value added until operator
+        if (opValue == '') {
+            firstValue = display.value;
+            console.log(firstValue);
+        }
+        // If first value and operator stored, second value added
+        else {
+            secondValue = display.value;
+            console.log(secondValue);
+        }
+    }
+}
+
+
+const calcContainer = document.getElementById('calculator');
+
+const display = document.getElementById('display-area');
+const buttons = document.querySelectorAll('button');
+
+// Event listener checks for every button
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        // If operator, display value is just operator
+        if (operators.includes(e.target.value)) {
+            display.value = e.target.value;
+            step = 1;
+        }
+        // If last operator, display value only next value
+        else if (step == 1) {
+            display.value = e.target.value
+            step = 0;        
+        }
+        else if (display.value != '') {
+            display.value += e.target.value
+        }
+        else {
+            display.value = e.target.value
+        }
+        
+        calulate(e.target.value);
+    });
+});
